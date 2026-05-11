2eaaa35170bafcfc757f00c3da6ed14c953d0cfe
NGZ101 <naufal.geraldo30@gmail.com>
2026-05-11 11:33:42 +0700

    Tambah Mainnn

--- 2311102154_Naufal Geraldo Putra P/flutter1/lib/mainn.dart
+++ 2311102154_Naufal Geraldo Putra P/flutter1/lib/mainn.dart
@@ -1,14 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-main() => runApp(flutter1());
+void main() {
+  runApp(const AplikasiPraktikum());
+}
 
-class flutter1 extends StatelessWidget {
+class AplikasiPraktikum extends StatelessWidget {
+  const AplikasiPraktikum({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Ini head"),),
-        body: Text("Ini body"),
+        appBar: AppBar(
+          title: const Text('Poster & Papan Skor'),
+          backgroundColor: Colors.blue,
+        ),
+        body: const PenghitungMahasiswa(),
+      ),
+    );
+  }
+}
+
+class PenghitungMahasiswa extends StatefulWidget {
+  const PenghitungMahasiswa({super.key});
+
+  @override
+  State<PenghitungMahasiswa> createState() =>
+      _PenghitungMahasiswaState();
+}
+
+class _PenghitungMahasiswaState
+    extends State<PenghitungMahasiswa> {
+
+  int jumlahHadir = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Jumlah yang Hadir:',
+            style: GoogleFonts.poppins(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            '$jumlahHadir',
+            style: const TextStyle(
+              fontSize: 60,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              setState(() {
+                jumlahHadir++;
+              });
+            },
+            child: const Text('Tambah Mahasiswa'),
+          ),
+        ],
       ),
     );
   }
