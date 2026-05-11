3af0d1dd6a94d5e355ea6610e36c4d9406e490cb
LuthfiAdiHarianto <2311102172@ittelkom-pwt.ac.id>
2026-05-12 00:52:14 +0700

    Menambahkan tugas pertemuan 10 mobile Luthfi Adi Harianto

--- 2311102227_Muhammad Rafiful Hana/flutter1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hans/Documents/Collage/Semester\ 6/Aplikasi\ Berbasis\ Platform/flutter1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/hans/Documents/Collage/Semester\ 6/Aplikasi\ Berbasis\ Platform/flutter1/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
--- aji_noto_sutrisno_2311102262/android/app/src/main/kotlin/com/example/aji_noto_sutrisno_2311102262/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.aji_noto_sutrisno_2311102262
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
